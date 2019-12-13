80c965e43c5ffd5b7259e67525a1cf47cdf7b4c9
arronf2e <191446367@qq.com>
2019-12-13 14:52:37 +0800

    组件添加子组件

--- React.js
+++ React.js
@@ -1,10 +1,16 @@
-function createElement(tagName, props, children) {
+function createElement(tagName, props, ...children) {
     // 函数组件
     if(typeof tagName === 'function') {
         return tagName()
     }else {
         let tag = document.createElement(tagName)
-        tag.innerHTML = children
+        children.forEach(child => {
+            if(typeof child === 'string') {
+                tag.innerHTML += child
+            }else if(typeof child === 'object'){
+                tag.appendChild(child)
+            }
+        })
         return tag
     }
 }
--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const HelloWorld = React.createElement('div', null, 'hello world')
 
+
+
 const Hello = function() {
     return React.createElement('div', null, 'hello component')
 }
+const HelloComponent = React.createElement(Hello, null, null)
 
-const HelloComponent = React.createElement(Hello, null, '')
 
-ReactDOM.render(HelloComponent, document.getElementById('app'))
+const Hello3 = function() {
+    return React.createElement('div', null, 'hello 3')
+}
+
+const Hello3_1 = React.createElement(Hello3, null, null)
+const Hello3_2 = React.createElement(Hello3, null, null)
+const Hello3_3 = React.createElement('div', null, 'hello3 inner html')
+// children 分别为组件， div， text
+const HelloComponent3 = React.createElement('div', null, 
+    Hello3_1,
+    Hello3_2,
+    Hello3_3,
+    'hello inner three'
+)
+
+ReactDOM.render(HelloComponent3, document.getElementById('app'))
